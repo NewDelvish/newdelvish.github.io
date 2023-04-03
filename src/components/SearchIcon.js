ce8c729253d5380f2c4eb9ddd5cf5f1390922f50
NewDelvish <129792128+NewDelvish@users.noreply.github.com>
2023-04-03 19:45:33 +0000

    new build

--- src/components/SearchIcon.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-import Search from "./search_icon.svg"
-export default class SearchIcon extends Component {
-  render() {
-    return (
-        <img src={Search} alt="Search" className='h-10' onError={e=>{e.target.src="https://raw.githubusercontent.com/KnownBlackHat/cdn-web-app/main/src/components/search_icon.svg"}} />
-    )
-  }
-}
